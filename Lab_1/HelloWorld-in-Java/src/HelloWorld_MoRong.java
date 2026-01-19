37929e632da4a98a2921b365ae0112113c37b1b8
Nguyen Thanh Dat <datnguyen.2782005@gmail.com>
2026-01-19 17:20:57 +0700

    Hello World in Java mo rong

---
+++ Lab_1/HelloWorld-in-Java/src/HelloWorld_MoRong.java
@@ -0,0 +1,12 @@
+public class HelloWorld_MoRong {
+
+    public static void main(String[] args) {
+
+        // Lệnh in ra màn hình
+        System.out.println("Xin chào thế giới Java!");
+        System.out.println("Chào mừng đến với lập trình Java");
+        System.out.print("Tôi là Nguyễn Thành Đạt");
+        System.out.println(" - MSSV: 65130423");
+
+    }
+}
