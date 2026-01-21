6f936ee4e6f8d94285f55d4bad952947ed1c1ba9
Nguyen Thanh Dat <datnguyen.2782005@gmail.com>
2026-01-21 21:24:30 +0700

    Complete Lab 1 - Exercise 2

--- Lab_1/Bai_2/src/Main.java
+++ Lab_1/Bai_2/src/Main.java
@@ -1,13 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Tinh chu vi Hinh chu nhat");
+        System.out.print("Nhap chieu dai: ");
+        double dai = scanner.nextDouble();
+        System.out.print("Nhap chieu rong: ");
+        double rong = scanner.nextDouble();
+        System.out.println("Chu vi Hinh chu nhat: " + (dai + rong)*2);
+
     }
+
 }
