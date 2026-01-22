efedbdc78bce1993d85770ba3e0c444d001d6d9d
Nguyen Thanh Dat <datnguyen.2782005@gmail.com>
2026-01-22 20:08:19 +0700

    Add Bai1/HelloWorld

---
+++ BaiTap/Bai_1_Cai_dat_moi_truong_va_vi_du_Hello_World/src/HelloWorld.java
@@ -0,0 +1,21 @@
+// Đây là chú thích một dòng
+
+/*
+ * Đây là chú thích nhiều dòng
+ * Chương trình đầu tiên: Hello World
+ */
+
+// Lớp chính của chương trình
+public class HelloWorld {
+    
+    // Phương thức main - điểm bắt đầu chương trình
+    public static void main(String[] args) {
+        
+        // Lệnh in ra màn hình
+        System.out.println("Xin chào thế giới Java!");
+        System.out.println("Chào mừng đến với lập trình Java");
+        
+        // In kết quả tính toán
+        System.out.println("5 + 3 = " + (5 + 3));
+    }
+}
