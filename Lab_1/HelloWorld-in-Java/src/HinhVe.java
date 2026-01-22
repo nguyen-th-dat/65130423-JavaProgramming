dd3669971ae4637a1aa61251c1e814f8189e76b0
Nguyen Thanh Dat <datnguyen.2782005@gmail.com>
2026-01-22 20:02:33 +0700

    delete folder HelloWorld-in-Java

--- Lab_1/HelloWorld-in-Java/src/HinhVe.java
+++
@@ -1,8 +0,0 @@
-public class HinhVe {
-    public static void main(String[] args) {
-        System.out.println("   *   ");
-        System.out.println("  ***  ");
-        System.out.println(" ***** ");
-        System.out.println("*******");
-    }
-}
