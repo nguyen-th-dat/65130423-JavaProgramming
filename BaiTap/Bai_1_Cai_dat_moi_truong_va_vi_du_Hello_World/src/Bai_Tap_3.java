2c06dd4be7af5f9215eb55ccf0efd5b39fcb93ff
Nguyen Thanh Dat <datnguyen.2782005@gmail.com>
2026-01-22 20:23:09 +0700

    submit Bai_1/Bai_Tap_3

--- BaiTap/Bai_1_Cai_dat_moi_truong_va_vi_du_Hello_World/src/Bai_Tap_3.java
+++ BaiTap/Bai_1_Cai_dat_moi_truong_va_vi_du_Hello_World/src/Bai_Tap_3.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Bai_Tap_3 {
+    public static void main(String[] args) {
+        // phep tinh 1 + 1
+        System.out.println("1 + 1 = " + (1 + 1));
+        System.out.println("");
+
+        // phep tinh 10 - 5
+        System.out.println("10 - 5 = " + (10 - 5));
+        System.out.println("");
+
+        // Bang cuu chuong 2
+        System.out.println("Bang cuu chuong 2");
+        for (int i = 1; i <= 10; i++){
+            System.out.println("2 x " + i +" = " + (2 * i));
+
+        }
+        System.out.println("");
+
+        // phep tinh 100 / 3
+        System.out.println("100 / 3 = " + (100.0 / 3));
+    }
+
 }
