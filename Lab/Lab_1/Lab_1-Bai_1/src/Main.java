8909ae9d6c46bac5119f85c354c4b53c2505c134
Nguyen Thanh Dat <datnguyen.2782005@gmail.com>
2026-01-22 20:40:48 +0700

    move Lab_1/ to Lab/

---
+++ Lab/Lab_1/Lab_1-Bai_1/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Họ và tên: ");
+        String hoTen = scanner.nextLine();
+        System.out.print("Điểm TB: ");
+        double diemTB = scanner.nextDouble();
+        while (diemTB < 0 || diemTB > 10)
+        {
+            System.out.print("Điểm TB phai nam trong khoan tu 0 den 10: ");
+            diemTB = scanner.nextDouble();
+        }
+//        System.out.printf("%s %f điểm", hoTen, diemTB);
+        System.out.println("Ho ten: " + hoTen + " Diem TB: " + diemTB);
+    }
+
+}
---
+++ Lab/Lab_1/README.md
@@ -0,0 +1,14 @@
+# Môn học: **Lập trình thiết bị di động**
+
+## Giới thiệu
+Lab 1
+
+
+## Cấu trúc thư mục
+```
+Lab_1/
+├── HelloWorld-in-Java/
+├── Lab_1-Bai-1/
+└── README.md
+```
+
