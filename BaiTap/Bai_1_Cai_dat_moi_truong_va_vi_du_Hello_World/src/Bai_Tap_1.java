10224b0a0c09898ad1f4ac894754970eebdde091
Nguyen Thanh Dat <datnguyen.2782005@gmail.com>
2026-01-22 20:12:04 +0700

    Add Bai1/Bai_Tap_1

---
+++ BaiTap/Bai_1_Cai_dat_moi_truong_va_vi_du_Hello_World/src/Bai_Tap_1.java
@@ -0,0 +1,11 @@
+public class Bai_Tap_1 {
+    public static void main(String[] args) {
+
+        // Lệnh in ra màn hình
+        System.out.println("Xin chào thế giới Java!");
+        System.out.println("Chào mừng đến với lập trình Java");
+
+        //In thong tin ca nhan
+        System.out.println("Tôi là Nguyễn Thành Đạt,  MSSV: " + 65130423);
+    }
+}
