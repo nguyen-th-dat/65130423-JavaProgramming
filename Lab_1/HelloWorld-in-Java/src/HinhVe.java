37a2c088bb4e233c5d13946183cf97960a2fda9b
Nguyen Thanh Dat <datnguyen.2782005@gmail.com>
2026-01-22 19:48:14 +0700

    Add HinhVe.java

---
+++ Lab_1/HelloWorld-in-Java/src/HinhVe.java
@@ -0,0 +1,8 @@
+public class HinhVe {
+    public static void main(String[] args) {
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
+    }
+}
