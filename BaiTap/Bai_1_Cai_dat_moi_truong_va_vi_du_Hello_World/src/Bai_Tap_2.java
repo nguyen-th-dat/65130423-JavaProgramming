5db0c69bce0421819aee856c7c6489817b99b48b
Nguyen Thanh Dat <datnguyen.2782005@gmail.com>
2026-01-22 20:12:55 +0700

    Add Bai1/Bai_Tap_2

---
+++ BaiTap/Bai_1_Cai_dat_moi_truong_va_vi_du_Hello_World/src/Bai_Tap_2.java
@@ -0,0 +1,8 @@
+public class Bai_Tap_2 {
+    public static void main(String[] args) {
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
+    }
+}
