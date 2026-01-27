7ecfb408992da9018402633064b7eb2832bf9247
Nguyen Thanh Dat <datnguyen.2782005@gmail.com>
2026-01-28 06:59:36 +0700

    Update Bai_Tap_2.java

--- BaiTap/Bai_1_Cai_dat_moi_truong_va_vi_du_Hello_World/src/Bai_Tap_2.java
+++ BaiTap/Bai_1_Cai_dat_moi_truong_va_vi_du_Hello_World/src/Bai_Tap_2.java
@@ -1,8 +1,26 @@
 public class Bai_Tap_2 {
     public static void main(String[] args) {
-        System.out.println("   *   ");
-        System.out.println("  ***  ");
-        System.out.println(" ***** ");
-        System.out.println("*******");
+        //old
+//        System.out.println("   *   ");
+//        System.out.println("  ***  ");
+//        System.out.println(" ***** ");
+//        System.out.println("*******");
+
+        //new
+        int n = 5; // Chieu cao
+
+        for (int i = 0; i < n; i++) {
+            // In khoang trang ben trai
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            // In dau *
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            // Xuong dong
+            System.out.println();
+        }
     }
 }
